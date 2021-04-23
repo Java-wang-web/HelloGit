180c868cf4925dd1379ac5145a37ab4908e50e75
wangshuming <Wangshuminga@yeah.net>
2021-04-23 21:20:12 +0800

    添加文件

---
+++ ershou/src/main/java/HelloGit.java
@@ -0,0 +1,11 @@
+
+
+/**
+ * @author jitwxs
+ * @date 2021Äê04ÔÂ23ÈÕ 21:09
+ */
+public class HelloGit {
+        public static void main(String[] args) {
+                System.out.println("hello git");
+        }
+}
