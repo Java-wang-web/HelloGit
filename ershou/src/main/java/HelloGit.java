97be96f77c3b6fcac7dbd987c3a561e3e89d00e2
wangshuming <Wangshuminga@yeah.net>
2021-04-23 21:29:06 +0800

    版本一

--- ershou/src/main/java/HelloGit.java
+++ ershou/src/main/java/HelloGit.java
@@ -7,5 +7,7 @@
 public class HelloGit {
         public static void main(String[] args) {
                 System.out.println("hello git");
+                System.out.println("�汾һ");
+               
         }
 }
